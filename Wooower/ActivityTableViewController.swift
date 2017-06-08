802da2d7f6d70b89a5bdf3ef57f1076b060eb26e
Vlad Chuvashov <vladchuvashov@gmail.com>
2017-06-08 13:36:55 +0300

    display data in container "ActivityTableViewController" from Parse

--- Wooower/ActivityTableViewController.swift
+++ Wooower/ActivityTableViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import Parse
 
 class ActivityTableViewController: UITableViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var pictureImageView: UIImageView!
+    
+    @IBOutlet weak var avatarView: UIView!
+
+    var activityItem: PFObject? {
+        didSet {
+            configTableView()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configTableView()
+    }
 
+    func configTableView () {
+        if let activity = activityItem {
+            if let createdDate = activity.createdAt {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd MMMM"
+                let useDate = dateFormatter.string(from: createdDate)
+                if let dateStr = dateLabel {
+                    dateStr.text = "posted " + useDate
+                }
+            }
+            if let descriptionLabel = descriptionLabel {
+                descriptionLabel.text = (activity["descriptions"] as! String)
+                if let user = activity["user"] as? PFUser {
+                    if let userName = user.username {
+                        nameLabel.text = userName
+                    }
+                }
+                if let imageFile = activity["picture"] as? PFFile {
+                    imageFile.getDataInBackground(block: { (data, error) in
+                        if let imageData = UIImage(data: data!) {
+                            self.pictureImageView.image = imageData
+                        }
+                        
+                    })
+                }
+            }
+            
+            if let avatarView = avatarView {
+                avatarView.layer.cornerRadius = 50
+                avatarView.layer.borderWidth = 3
+                avatarView.layer.borderColor = UIColor.white.cgColor
+            }
+            
+        }
     }
 
    
--- Wooower/ActivityViewController.swift
+++ Wooower/ActivityViewController.swift
@@ -12,74 +12,22 @@ import ParseUI
 
 class ActivityViewController: UIViewController {
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var pictureImageView: UIImageView!
-
-    @IBOutlet weak var pictureView: UIView!
-
-    var activityItem: PFObject? {
-        didSet {
-            configView()
+    var activityItem: PFObject?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ActivityTableViewController" {
+            if let activityTable = segue.destination as? ActivityTableViewController {
+                activityTable.activityItem = self.activityItem
+            }
         }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configView()
     }
     
-    func configView () {
-        if let activity = activityItem {
-            if let createdDate = activity.createdAt {
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "dd MMMM"
-                let useDate = dateFormatter.string(from: createdDate)
-                if let dateStr = dateLabel {
-                    dateStr.text = "posted " + useDate
-                }
-            }
-            if let descriptionLabel = descriptionLabel {
-                descriptionLabel.text = (activity["descriptions"] as! String)
-                if let user = activity["user"] as? PFUser {
-                    if let userName = user.username {
-                        nameLabel.text = userName
-                    }
-                }
-                if let imageFile = activity["picture"] as? PFFile {
-                    imageFile.getDataInBackground(block: { (data, error) in
-                        if let imageData = UIImage(data: data!) {
-                            self.pictureImageView.image = imageData
-                        }
-                        
-                    })
-                }
-            }
-            
-            if let pictureView = pictureView {
-                pictureView.layer.cornerRadius = 50
-                pictureView.layer.borderWidth = 3
-                pictureView.layer.borderColor = UIColor.white.cgColor
-            }
-            
-        }
-    }
 
 }
 
-//extension ActivityViewController: UITableViewDataSource, UITableViewDelegate {
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 2
-//    }
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)
-//        return
-//    }
-//}
 
 
 
