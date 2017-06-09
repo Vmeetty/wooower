e6d920b71e72a44a43746241c7043e35432aa5a0
Vlad Chuvashov <vladchuvashov@gmail.com>
2017-06-09 09:23:30 +0300

    try to display one comment

--- Wooower/ActivityTableViewController.swift
+++ Wooower/ActivityTableViewController.swift
@@ -8,15 +8,35 @@
 
 import UIKit
 import Parse
+import ParseUI
 
 class ActivityTableViewController: UITableViewController {
 
+    @IBOutlet var myTableView: UITableView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var pictureImageView: UIImageView!
     
     @IBOutlet weak var avatarView: UIView!
+    
+    // comments items
+    
+    @IBOutlet weak var firstCommentAvatarView: UIView!
+    @IBOutlet weak var firstCommentAvatarImageView: UIImageView!
+    @IBOutlet weak var firstCommentNameLabel: UILabel!
+    @IBOutlet weak var firstCommentDescriptionLabel: UILabel!
+    @IBOutlet weak var secondCommentAvatarView: UIView!
+    @IBOutlet weak var secondCommentAvatarImageView: UIImageView!
+    @IBOutlet weak var secondCommentNameLabel: UILabel!
+    @IBOutlet weak var secondCommentDescriptionLabel: UILabel!
+    @IBOutlet weak var thirdCommentAvatarView: UIView!
+    @IBOutlet weak var thirdCommentAvatarImageView: UIImageView!
+    @IBOutlet weak var thirdCommentNameLabel: UILabel!    
+    @IBOutlet weak var thirdCommentDescriptionLabel: UILabel!
+    
+    
+    
 
     var activityItem: PFObject? {
         didSet {
@@ -27,6 +47,7 @@ class ActivityTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configTableView()
+        myTableView.keyboardDismissMode = .onDrag
     }
 
     func configTableView () {
@@ -54,6 +75,24 @@ class ActivityTableViewController: UITableViewController {
                         
                     })
                 }
+                let commentRelation = activityItem?.relation(forKey: "comments")
+                let query = commentRelation?.query()
+                query?.includeKey("user")
+                query?.findObjectsInBackground(block: { (comments, error) in
+                    if let comments = comments {
+                        var nickNameStr = ""
+                        var textStr = ""
+                        comments.forEach({ (object) in
+                            if let username = (object["user"] as? PFUser)?.username {
+                                nickNameStr = username
+                            }
+                            let text = object["text"] as! String
+                            textStr = text
+                        })
+                        self.firstCommentNameLabel.text = nickNameStr
+                        self.firstCommentDescriptionLabel.text = textStr
+                    }
+                })
             }
             
             if let avatarView = avatarView {
@@ -65,5 +104,8 @@ class ActivityTableViewController: UITableViewController {
         }
     }
 
+    @IBAction func allCommentsAction(_ sender: UIButton) {
+    }
+    
    
 }
--- Wooower/ActivityViewController.swift
+++ Wooower/ActivityViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 import Parse
-import ParseUI
+
 
 class ActivityViewController: UIViewController {
 
-    var activityItem: PFObject?
+    var activityItem: PFObject?    
+    @IBOutlet weak var commentText: UITextView!
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ActivityTableViewController" {
@@ -24,7 +25,23 @@ class ActivityViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
     
+    @IBAction func sendCommentAction(_ sender: UIButton) {
+        let comment = PFObject(className: "Comment")
+        comment["text"] = commentText.text
+        comment["user"] = PFUser.current()
+        comment["post"] = self.activityItem
+        comment.saveInBackground { (succes, error) in
+            let relation = self.activityItem?.relation(forKey: "comments")
+            relation?.add(comment)
+            self.activityItem?.saveInBackground(block: { (succes, error) in
+                print("upload \(succes)")
+            })
+        }
+    }
 
 }
 
